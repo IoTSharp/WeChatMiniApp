829d87ceaf96fb75e9ac23a43fd6a8c0775c04e0
qian wang <1510106069@qq.com>
2022-08-31 23:37:55 +0800

    feat: index page layout

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,17 +1,23 @@
 <template>
   <view class="index">
     <Banner />
+    <Device />
+    <Events />
   </view>
 </template>
 
 <script>
 import './index.scss'
-import Banner from '../../components/Banner'
+import Banner from './components/Banner'
+import Device from './components/Device'
+import Events from './components/Events'
 
 export default {
   name: 'Index',
   components: {
-    Banner
+    Banner,
+    Events,
+    Device
   }
 }
 </script>
