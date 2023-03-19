e5786e10c22f982cf525eacdb28ae54e0bd589e2
qian wang <1510106069@qq.com>
2023-03-19 21:10:44 +0800

    feat: 关于我们 页面布局

---
+++ src/pages/aboutUs/index.tsx
@@ -0,0 +1,23 @@
+import { Image, View } from "@tarojs/components";
+import { FC } from "react";
+import styles from "./index.module.scss";
+
+export interface IAboutUsProps {}
+
+const AboutUs: FC<IAboutUsProps> = ({}) => {
+  return (
+    <View className={styles.aboutUsContainer}>
+      <View className={styles.logo}>
+        <Image
+          className={styles.icon}
+          src="https://oweqian.oss-cn-hangzhou.aliyuncs.com/miniApp/logo-icon.svg"
+        />
+      </View>
+      <View className={styles.description}>连接物理设备与数字世界</View>
+      <View className={styles.version}>IoTSharp V 1.0.0</View>
+      <View className={styles.copyright}>© 2018 - 2023 The IoTSharp Authors. All rights reserved.</View>
+    </View>
+  );
+};
+
+export default AboutUs;
