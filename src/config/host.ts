77ae7d34bcc838280c18d77658a935ed23c2dadb
OweQian <1510106069@qq.com>
2023-03-29 17:42:23 +0800

    fix: request域名替换

--- src/config/host.ts
+++ src/config/host.ts
@@ -1,9 +1,9 @@
 const hosts = {
-  local: 'https://cloud.iotsharp.net',
-  dev: 'https://cloud.iotsharp.net',
-  test: 'https://cloud.iotsharp.net',
-  pre: 'https://cloud.iotsharp.net',
-  prod: 'https://cloud.iotsharp.net',
+  local: 'https://demo.iotsharp.net',
+  dev: 'https://demo.iotsharp.net',
+  test: 'https://demo.iotsharp.net',
+  pre: 'https://demo.iotsharp.net',
+  prod: 'https://demo.iotsharp.net',
 };
 
 export default hosts;
